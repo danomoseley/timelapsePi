314cc400ab1110b45e92e2381be95b9aba20addd
Dan Moseley <danomoseley@gmail.com>
2016-01-26 17:10:44 -0500

    Create timelapse in tmp before moving to destination folder

--- makeHourlyVideo.sh
+++ makeHourlyVideo.sh
@@ -24,15 +24,17 @@ find $path/*-ae3.jpg -mmin -60 -type f | while read f; do
     i=$((i+1))
 done
 
-ERROR="$(mencoder mf://$temp_folder/*.jpg -nosound -o $destination/$date_hour.mp4 -vf scale=480:-10,harddup -lavfopts format=mp4 -faacopts mpeg=4:object=2:raw:br=128 -oac faac -ovc x264 -sws 9 -x264encopts nocabac:level_idc=30:bframes=0:global_header:threads=auto:subq=5:frameref=6:partitions=all:trellis=1:chroma_me:me=umh:bitrate=500 -of lavf -mf fps=12 2>&1 > /dev/null)"
+ERROR="$(mencoder mf://$temp_folder/*.jpg -nosound -o /tmp/$date_hour.mp4 -vf scale=480:-10,harddup -lavfopts format=mp4 -faacopts mpeg=4:object=2:raw:br=128 -oac faac -ovc x264 -sws 9 -x264encopts nocabac:level_idc=30:bframes=0:global_header:threads=auto:subq=5:frameref=6:partitions=all:trellis=1:chroma_me:me=umh:bitrate=500 -of lavf -mf fps=12 2>&1 > /dev/null)"
 
 rm -R $temp_folder
 
 OUT=$?
 if [ $OUT -ne 0 ];then
     echo $ERROR
+    rm /tmp/$date_hour.mp4
     exit $OUT
 else
+    mv /tmp/$date_hour.mp4 $destination
     scp -pr $destination/$date_hour.mp4 $REMOTE_SERVER_HOST:$REMOTE_SERVER_PATH/latest_vid.mp4
 fi
 
