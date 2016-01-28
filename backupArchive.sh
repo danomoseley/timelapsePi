8e590e6a0c90eacce74284cc00a5331f5f24b980
Dan Moseley <danomoseley@gmail.com>
2016-01-28 17:19:38 -0500

    Continue backing up to secondary remote server even if first fails

--- backupArchive.sh
+++ backupArchive.sh
@@ -3,6 +3,8 @@
 DIR=$(cd $(dirname $0); pwd -P)
 source $DIR/config.cfg
 
+set +e
+
 rsync -a --bwlimit=$RSYNC_BWLIMIT $DIR/archive/ $REMOTE_SERVER_HOST:$REMOTE_SERVER_PATH/archive
 
 if [ -v REMOTE_BACKUP_SERVER_HOST ]; then
