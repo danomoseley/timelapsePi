ffd86d84222bf1b3332580e90e8c3b4d726d0ac4
Dan Moseley <danomoseley@gmail.com>
2016-01-28 17:18:43 -0500

    Backup archive to secondary remote server

--- backupArchive.sh
+++ backupArchive.sh
@@ -4,3 +4,7 @@ DIR=$(cd $(dirname $0); pwd -P)
 source $DIR/config.cfg
 
 rsync -a --bwlimit=$RSYNC_BWLIMIT $DIR/archive/ $REMOTE_SERVER_HOST:$REMOTE_SERVER_PATH/archive
+
+if [ -v REMOTE_BACKUP_SERVER_HOST ]; then
+    rsync -a --bwlimit=$RSYNC_BWLIMIT $DIR/archive/ $REMOTE_BACKUP_SERVER_HOST:$REMOTE_BACKUP_SERVER_PATH/archive
+fi
