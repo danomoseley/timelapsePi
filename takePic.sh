cd683d313a1b769ef2d404c64a16114e0f5071b5
Dan Moseley <danomoseley@gmail.com>
2016-04-15 11:13:54 -0400

    Set max runtime on ffmpeg and kill process if it runs too long Retry taking up to 5 photos if error is encountered

--- takePic.sh
+++ takePic.sh
@@ -16,36 +16,54 @@ fi
 filename=$destination/$timestamp.jpg
 
 failed_pics=0
-while [ $failed_pics -lt 5 ]
-do
-   ERROR="$(ffmpeg -f video4linux2 -s $RESOLUTION -i /dev/video0 -ss 0:0:5 -frames 1 $filename 2>&1 > /dev/null)"
+while [ $failed_pics -lt 5 ]; do
+   ERROR_FILE=/tmp/ffmpeg${timestamp}
+   { ffmpeg -f video4linux2 -s $RESOLUTION -i /dev/video0 -ss 0:0:5 -frames 1 $filename > $ERROR_FILE.log 2>&1 ; echo "$?" > $ERROR_FILE.response ; } &
+   pid=$!
+   sleep 10
+   kill -9 $pid > /dev/null 2>&1
+   status=$?
+   if [ $status -eq 0 ]; then 
+      OUT=124
+      ERROR=''
+   else
+      OUT=`cat $ERROR_FILE.response`
+      ERROR=`cat $ERROR_FILE.log`
+      rm $ERROR_FILE.log $ERROR_FILE.response
+   fi
 
-   OUT=$?
-   if [ $OUT -ne 0 ]
-   then
-      rm $filename
+   if [ $OUT -ne 0 ]; then
+      if [ -f $filename ]; then
+         rm $filename
+      fi
 
-      echo $ERROR
-      if [[ "$ERROR" == *"Input/output error" ]] || [[ "$ERROR" == *"Device or resource busy" ]]
-      then
+      if [ $OUT -eq 124 ]; then
+         echo "Capture timed out"
+      elif [[ "$ERROR" == *"Input/output error" ]]; then
          # Root cron runs script every minute which looks for /tmp/reboot.now
          # If file is found, system is rebooted
+         echo "Input/output error, rebooting"
          echo REBOOT > /tmp/reboot.now
+         exit $OUT
+      elif [[ "$ERROR" == *"Device or resource busy" ]]; then
+         echo "Device or resource busy"
+      else
+         echo $ERROR
       fi
-      exit $OUT
    else
       file_size=$(wc -c <"$filename")
-      if [ $file_size -le $MINIMUM_FILE_SIZE ]
-      then
+      if [ $file_size -le $MINIMUM_FILE_SIZE ]; then
          rm $filename
-         failed_pics=$(( $failed_pics + 1 ))
       else
          if [[ $((10#$minute % $PIC_SYNC_INTERVAL)) -eq 0 || $1 -eq $minute ]]
          then
             scp -pr $filename $REMOTE_SERVER_HOST:$REMOTE_SERVER_PATH/latest_pic.jpg
+            source $DIR/backupArchive.sh
          fi
          exit 0
       fi
    fi
+   failed_pics=$(( $failed_pics + 1 ))
+   sleep 5
 done
 echo "No valid pics after 5 attempts"
