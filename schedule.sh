22714f95242b4231eb57eb76a1c6473cad5a44dd
Dan Moseley <danomoseley@gmail.com>
2016-04-15 11:12:17 -0400

    Location city and state in config.cfg for sunrise/sunset lookup

--- schedule.sh
+++ schedule.sh
@@ -7,9 +7,13 @@ command="$DIR/takePic.sh"
 daily_timelapse_command="$DIR/makeTimelapse.sh daily"
 hourly_timelapse_command="$DIR/makeTimelapse.sh hourly"
 
-result=$(curl -s http://weather.yahooapis.com/forecastrss?w=$YAHOO_WEATHER_ID|grep astronomy| awk -F\" '{print $2 " " $4;}')
+url="https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunrise%2C%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${YAHOO_WEATHER_CITY}%2C%20${YAHOO_WEATHER_STATE}%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+result=$(curl -s $url)
+sunrise=$(echo $result | grep -Po 'sunrise="\K[0-9:]*')
+sunset=$(echo $result | grep -Po 'sunset="\K[0-9:]*')
 
-read sunrise sunrise_a sunset sunset_a <<<$(echo $result)
+echo $sunrise
+echo $sunset
 
 IFS=':' read sunrise_hour sunrise_minute <<< "$sunrise"
 sunrise_hour=$((10#$sunrise_hour))
