8e9f0d3d26305066c4b7542393aaa9f9318a37fc
Dan Moseley <danomoseley@gmail.com>
2016-11-10 15:10:37 -0500

    Touch controls for archive: swipe and hold to play timelapse at 60fps (1 hour per second)

--- web/js/scroll.js
+++ web/js/scroll.js
@@ -1,12 +1,18 @@
 date=parseInt(moment().format('YYMMDD'));
 sorted_files=[];
+touch = false;
 
 $(document).ready(function() {
    $("body").on("swipeleft",function(){
-      loadNextPic();
+      loadPreviousPic();
    });
    $("body").on("swiperight",function(){
-      loadPreviousPic();
+      loadNextPic();
+   });
+   $("body").bind("touchstart", function(){
+       touch = true;
+   }).bind("touchend", function(){
+       touch = false;
    });
    window.addEventListener('mousewheel', function(e){
       if (e.wheelDelta > 0) {
@@ -33,21 +39,25 @@ function loadCurentPic() {
 function loadNextPic() {
    if (current_index < sorted_files.length-1) {
       current_index = current_index + 1;
-      loadPic(current_index);
+      loadPic(current_index, loadNextPic);
    }
 }
 
 function loadPreviousPic() {
    if (current_index > 0) {
       current_index = current_index - 1;
-      loadPic(current_index);
+      loadPic(current_index, loadPreviousPic);
    }
 }
 
-function loadPic(i) {
+function loadPic(i, callback) {
    imgPath = '/archive/photo/'+date+'/'+sorted_files[i]
    $('<img src="'+ imgPath +'">').load(function() {
       $('#container img').remove();
       $(this).appendTo('#container');
+      if (touch && callback !== undefined) {
+         setTimeout(callback, 17); // 60fps - 1 hour in timelapse per second 
+      }
    });
 }
+
