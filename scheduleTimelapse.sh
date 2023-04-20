973f163b7fc850d81e756ff4c35dba7ad97435fb
Dan Moseley <danomoseley@gmail.com>
2023-04-19 20:16:08 -0400

    Update scheduleTimelapse.sh

--- scheduleTimelapse.sh
+++ scheduleTimelapse.sh
@@ -24,35 +24,46 @@ echo "Sunset: ${sunset}pm"
 
 IFS=':' read sunrise_hour sunrise_minute <<< "$sunrise"
 sunrise_hour=$((10#$sunrise_hour))
+sunrise_range_start_hour=$sunrise_hour
 sunrise_minute=$((10#$sunrise_minute))
-sunrise_minute=$((sunrise_minute-SUNRISE_SUNSET_BUFFER_MINUTES))
-if ((sunrise_minute < 0)); then
-    sunrise_minute=$((60+sunrise_minute))
-    sunrise_hour=$((sunrise_hour-1))	
+sunrise_range_start_minute=$((sunrise_minute-SUNRISE_SUNSET_BUFFER_MINUTES))
+if ((sunrise_range_start_minute < 0)); then
+    sunrise_range_start_minute=$((60+sunrise_range_start_minute))
+    sunrise_range_start_hour=$((sunrise_range_start_hour-1))	
 fi
 
 
 IFS=':' read sunset_hour sunset_minute <<< "$sunset"
 sunset_hour=$((sunset_hour+12))
+sunset_range_end_hour=$sunset_hour
 sunset_minute=$((10#$sunset_minute))
-sunset_minute=$((sunset_minute+SUNRISE_SUNSET_BUFFER_MINUTES+SUNSET_EXTRA_BUFFER_MINUTES))
-sunset_minute=$(echo "($sunset_minute + 9) / 10 * 10" | bc)
-if ((sunset_minute > 59)); then
-    sunset_minute=$((sunset_minute-60))
-    sunset_hour=$((sunset_hour+1))
+sunset_range_end_minute=$((sunset_minute+SUNRISE_SUNSET_BUFFER_MINUTES-10+SUNSET_EXTRA_BUFFER_MINUTES))
+sunset_range_end_minute=$(echo "($sunset_range_end_minute + 9) / 10 * 10" | bc)
+if ((sunset_range_end_minute > 59)); then
+    sunset_range_end_minute=$((sunset_range_end_minute-60))
+    sunset_range_end_hour=$((sunset_range_end_hour+1))
 fi
 
-cron_hour_start=$((sunrise_hour+1))
-cron_hour_end=$((sunset_hour-1))
+sunset_finale_minute=$((sunset_range_end_minute+10))
+sunset_finale_hour=$sunset_range_end_hour
+if ((sunset_finale_minute > 59)); then
+    sunset_finale_minute=$((sunset_finale_minute-60))
+    sunset_finale_hour=$((sunset_finale_hour+1))
+fi
 
-sunrise_cron="$sunrise_minute-59/10 $sunrise_hour * * * $make_command #SUNRISE_COMMAND"
-sunset_cron="0-$sunset_minute/10 $sunset_hour * * * $make_command sunset #SUNSET_COMMAND"
-range_cron="*/10 $cron_hour_start-$cron_hour_end * * * $make_command #RANGE_COMMAND"
+range_hour_start=$((sunrise_range_start_hour+1))
+range_hour_end=$((sunset_range_end_hour-1))
+
+sunrise_range_cron="$sunrise_range_start_minute-59/10 $sunrise_range_start_hour * * * $make_command #SUNRISE_RANGE_COMMAND"
+sunset_range_cron="0-$sunset_range_end_minute/10 $sunset_range_end_hour * * * $make_command #SUNSET_RANGE_COMMAND"
+sunset_finale_cron="$sunset_finale_minute $sunset_finale_hour * * * $make_command sunset #SUNSET_FINALE_COMMAND"
+range_cron="*/10 $range_hour_start-$range_hour_end * * * $make_command #RANGE_COMMAND"
 
 cron=$(crontab -l)
-cron=$(sed "s,.*#SUNRISE_COMMAND$,$sunrise_cron,g" <<< "$cron")
+cron=$(sed "s,.*#SUNRISE_RANGE_COMMAND$,$sunrise_range_cron,g" <<< "$cron")
 cron=$(sed "s,.*#RANGE_COMMAND$,$range_cron,g" <<< "$cron")
-cron=$(sed "s,.*#SUNSET_COMMAND$,$sunset_cron,g" <<< "$cron")
+cron=$(sed "s,.*#SUNSET_RANGE_COMMAND$,$sunset_range_cron,g" <<< "$cron")
+cron=$(sed "s,.*#SUNSET_FINALE_COMMAND$,$sunset_finale_cron,g" <<< "$cron")
 
 echo "$cron" | crontab -
 
